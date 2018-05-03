33aa6b9666b8a9bfbff6e0bd5589a0aaa66c39fb
Avinash <atahakik@asu.edu>
2018-05-02 23:27:14 -0700

    analyzer, shell code generator, input sanitization
    Signed-off-by: Avinash <atahakik@asu.edu>

---
+++ code_analyzer.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd) {
+    std::array<char, 1028> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+            result += buffer.data();
+    }
+    return result;
+}
+
+int main() {
+	exec("readelf -a a.out");
+	return 0;
+}
