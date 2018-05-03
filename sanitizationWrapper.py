f227a08d9eb216c0b9657bf5f25e39842bfca5ad
Abhi <asmarat1@asu.edu>
2018-05-02 22:56:51 -0700

    sanitizationWrapper changed

--- sanitizationWrapper.py
+++ sanitizationWrapper.py
@@ -2,10 +2,8 @@ from pwn import *
 import sys
 import re
 def input_sanitization(input_string):
-	if re.compile("[; | \| | \& | \$]", input_string):
-		return " "
-	else:
-		return input_string
+	input_string = re.sub(r"\W",'',input_string)
+	return input_string
 sh = process(sys.argv[1])
 msg1 = sh.recv(timeout=1)
 while msg1 != None:
@@ -13,10 +11,11 @@ while msg1 != None:
 	try:
 		input1 = raw_input()
 		#input1 = input_sanitization(str(input1))
-		print input1
-		sh.send(input1+"\n")
+		#print input1
+		sh.sendline(input1)
 		msg1 = sh.recv()
-	except (RuntimeError):
+		
+	except (RuntimeError, TypeError, EOFError):
 		print "Exception"
 		exit(0)
 sh.close()
